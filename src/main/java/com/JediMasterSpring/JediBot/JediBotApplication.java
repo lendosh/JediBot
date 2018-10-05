b05885118375138bcbc1572f85f08c35a36ea357
lendosh <vladyslav.pomazanov@gmail.com>
2018-10-05 12:46:44 +0300

    First commit

---
+++ src/main/java/com/JediMasterSpring/JediBot/JediBotApplication.java
@@ -0,0 +1,12 @@
+package com.JediMasterSpring.JediBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JediBotApplication.class, args);
+	}
+}
